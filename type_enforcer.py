d45e84ef79ba5e639f1d8bf6629741916e802a19
Shane Bellone <40317618+shanebellone@users.noreply.github.com>
2022-08-29 10:00:44 -0400

    Update type_enforcer.py
    added comments

--- type_enforcer.py
+++ type_enforcer.py
@@ -3,11 +3,16 @@ import typing
 
 def enforce_type(func):
     def wrapper_func(*args):
+        # Get type hints from decorated func
         expected_types = typing.get_type_hints(func)
+        # Build dictionary with comprehension with if evaluation
         type_errors = {arg: expected for arg, expected in zip(args, expected_types.values()) if type(arg) != expected}
+        # Check for errors
         if len(type_errors) == 0:
+            # If types match, return decorated function
             func(*args)
         else:
+            # Otherwise, raise TypeError
             error = "\tCheck {func}{args}: \t{errors}".format(func=func.__name__, args=args, errors=type_errors)
             raise TypeError(error)
 
